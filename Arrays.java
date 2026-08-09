1f5d6fc095253faaba368e722ad216287def5590
yalibali110 <yusufplus1@gmail.com>
2026-08-08 20:13:56 -0400

    Create Arrays.java
    Basic array practice

---
+++ Arrays.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize an array of integers
+        String[] animals = {"dog", "cat", "cow", "pig", "frog"};
+
+        // Access and print elements in the array
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println("Element at index " + i + ": " + animals[i]);
+        }
+    }
+}
